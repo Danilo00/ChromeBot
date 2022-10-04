ff574c1f8d9ebbcf685d90cc12c5854cca1ccef2
Danilo.Cappabianca <danilocappabianca@gmail.com>
2022-10-04 15:33:26 +0200

    Aggiungere i file di progetto.

---
+++ ChromeBot by DC/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace ChromeBot_by_DC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProxyList = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnUrlList = new System.Windows.Forms.Button();
+            this.nThreads = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nVisitsUrl = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nThreads)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnProxyList
+            // 
+            this.btnProxyList.Location = new System.Drawing.Point(199, 12);
+            this.btnProxyList.Name = "btnProxyList";
+            this.btnProxyList.Size = new System.Drawing.Size(184, 35);
+            this.btnProxyList.TabIndex = 0;
+            this.btnProxyList.Text = "Proxy List";
+            this.btnProxyList.UseVisualStyleBackColor = true;
+            this.btnProxyList.Click += new System.EventHandler(this.btnProxyList_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 118);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(371, 48);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnUrlList
+            // 
+            this.btnUrlList.Location = new System.Drawing.Point(9, 12);
+            this.btnUrlList.Name = "btnUrlList";
+            this.btnUrlList.Size = new System.Drawing.Size(184, 35);
+            this.btnUrlList.TabIndex = 2;
+            this.btnUrlList.Text = "Url List";
+            this.btnUrlList.UseVisualStyleBackColor = true;
+            this.btnUrlList.Click += new System.EventHandler(this.btnUrlList_Click);
+            // 
+            // nThreads
+            // 
+            this.nThreads.Location = new System.Drawing.Point(263, 53);
+            this.nThreads.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nThreads.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nThreads.Name = "nThreads";
+            this.nThreads.Size = new System.Drawing.Size(120, 22);
+            this.nThreads.TabIndex = 3;
+            this.nThreads.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Threads";
+            // 
+            // nVisitsUrl
+            // 
+            this.nVisitsUrl.Location = new System.Drawing.Point(263, 81);
+            this.nVisitsUrl.Name = "nVisitsUrl";
+            this.nVisitsUrl.Size = new System.Drawing.Size(120, 22);
+            this.nVisitsUrl.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Visits";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 178);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nVisitsUrl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nThreads);
+            this.Controls.Add(this.btnUrlList);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnProxyList);
+            this.Name = "Form1";
+            this.Text = "ChromeBot by DC";
+            ((System.ComponentModel.ISupportInitialize)(this.nThreads)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProxyList;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnUrlList;
+        private System.Windows.Forms.NumericUpDown nThreads;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nVisitsUrl;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ChromeBot by DC/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.Timers;
+
+namespace ChromeBot_by_DC
+{
+    public partial class Form1 : Form
+    {
+        string chromedriver_path = Environment.CurrentDirectory;
+        //string chromedriver_path = @"C:\Users\danilo.cappabianca\source\repos\ChromeBot by DC";
+        List<string> urlList = new List<string>();
+        List<string> proxyList = new List<string>();
+        int visit = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(File.Exists(chromedriver_path + "\\chromedriver.exe"))
+            {
+                if (urlList.Count > 0) //don't execute if UrlList is not loaded yet
+                {
+                    try
+                    {
+                        visit = 0;
+                        ChromeOptions options = new ChromeOptions();
+                        /*
+                         * You can add every options argument you need. More info in Selenium documents.
+                         * some examples here:
+                        options.AddArgument("--proxy-server=" + string_proxy);
+                        options.AddArgument("--silent");
+                        options.AddArgument("--log-level=3");
+                        options.AddArgument("--lang=en");
+                        options.AddArgument("--disable-logging");
+                        options.AddArgument("--no-sandbox");
+                        options.AddArgument("--disable-infobars");
+                        options.AddArgument("--ignore-certificate-errors");
+                        options.AddArgument("--disable-dev-shm-usage");
+                        options.AddUserProfilePreference("profile.default_content_setting_values.images", 2);
+                        */
+                        for (int i = 0; i < nThreads.Value; i++)
+                        {
+                            //select a different random URL from List every thread (can be only one too)
+                            Random random = new Random();
+                            int r = random.Next(0, urlList.Count - 1);
+                            string url = urlList[r];
+
+                            //select a different random PROXY from List every thread (if exist)
+                            string proxy = string.Empty;
+                            if (proxyList.Count > 0)
+                            {
+                                r = random.Next(0, proxyList.Count - 1);
+                                proxy = proxyList[r];
+                            }
+
+                            //Run each thread with different configuration                    
+                            Thread thread = new Thread(() => OpenUrl(options, chromedriver_path, url, proxy))
+                            {
+                                IsBackground = true
+                            };
+                            thread.Start();
+                            //System.Threading.Thread.Sleep(2 * 1000);               
+                        }
+                    }
+                    catch (Exception ex){ File.AppendAllText("errorLog.txt",DateTime.Now + " | " + ex.Message); }
+                }
+                else MessageBox.Show("You need to upload at least one URL","URL List is missing!",MessageBoxButtons.OK,MessageBoxIcon.Warning);              
+            }
+            else MessageBox.Show("Chromedriver.exe is missing in the executable file same folder.\nMake sure you have installed latest version for your browser.\nIf you need you can download at: https://chromedriver.chromium.org/downloads","Chromedriver is missing!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+        /// <summary>
+        /// Run Chrome Browser with declared options.
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="chromedriver_path">Without this driver this software can't work. Make sure you have installed latest version for your browser.If you need you can download at: https://chromedriver.chromium.org/downloads</param>
+        /// <param name="url">Link</param>
+        /// <param name="proxy">IP</param>
+        public void OpenUrl(ChromeOptions options, string chromedriver_path, string url, string proxy)
+        {
+            int nVisits = urlList.Count; //set default of visits equal as number of URLs (if user forgot to set)
+            if (proxy != string.Empty) options.AddArgument("--proxy-server=" + proxy);
+            try
+            {
+                if (nVisitsUrl.Text != string.Empty)
+                {
+                    if (nVisitsUrl.Text != string.Empty)
+                        nVisits = int.Parse(nVisitsUrl.Text);                    
+                    while (visit < nVisits) 
+                    {
+                        ++visit;
+                        IWebDriver Driver = new ChromeDriver(chromedriver_path, options);
+                        Driver.Navigate().GoToUrl(url);
+                        Driver.Dispose();
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void btnUrlList_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog1.Filter = "Txt Files (*.txt)|*.txt;";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                urlList.AddRange(File.ReadAllLines(openFileDialog1.FileName));
+            }
+        }
+
+        private void btnProxyList_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog1.Filter = "Txt Files (*.txt)|*.txt;";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                proxyList.AddRange(File.ReadAllLines(openFileDialog1.FileName));
+            }
+        }
+    }
+}
